7d0663f7b4e48d12ed31ceeff5b5a6cc2e8ccac0
DSRST106062223 <panther16590@gmail.com>
2018-10-06 22:03:20 +0800

    3

--- main.cpp
+++ main.cpp
@@ -4,13 +4,15 @@
 #include <time.h>
 #include <math.h>
 #include <list>
+#include <string.h>
 //#include <stdlib.h>
-//#include <string.h>
 
 using namespace std;
 
+char ifileDir[50];
+char ofileDir[50];
 
-void TheF(char* dir) {
+void Naive(const char* dir) {
     int height, width, total;
     int** matrix;
     int* head;
@@ -23,8 +25,13 @@ void TheF(char* dir) {
     ofstream ofile;
 
     string num = dir;
-    string ifileDir = "106062223/matrix.data";
-    string ofileDir = "106062223/final.peak";
+
+
+	strcat(ifileDir, dir);
+	strcat(ifileDir, "/matrix.data");
+
+	strcat(ofileDir, dir);
+	strcat(ofileDir, "/final.peak");
 
 
     ifile.open(ifileDir, ios::in);
@@ -47,10 +54,6 @@ void TheF(char* dir) {
         //getchar();
         ifile >> head[i];
     }
-
-
-
-
 
 
     { // top row
@@ -164,19 +167,284 @@ void TheF(char* dir) {
 }
 
 
+void BigRow(const char* dir) {
+    unsigned int height, width, height_1, width_1;
+    int* head;
+    //list<pair<int, int>> ans;
+    list<int> ans_i;
+    list<int> ans_j;
+
+
+    ifstream ifile;
+    ofstream ofile;
+
+    string num = dir;
+
+
+	strcat(ifileDir, dir);
+	strcat(ifileDir, "/matrix.data");
+
+	strcat(ofileDir, dir);
+	strcat(ofileDir, "/final.peak");
+
+
+    ifile.open(ifileDir, ios::in);
+    ofile.open(ofileDir, ios::out);
+
+
+
+    //fscanf(ifile, "%d %d\n", &height, &width);
+    ifile >> height >> width;
+    height_1 = height-1;
+    width_1 = width-1;
+
+    head = new int[width*3];
+    // Access element in this format:
+    // head[ row + col*3 ]
+
+
+
+    { /// Topmost row
+        // read first two row
+        for(int i=0; i<2; i++) {
+            for(int j=0; j<width; j++) {
+                ifile >> head[ i+(j*3) ];
+            }
+        }
+
+        // top left
+        if(head[0 + 0] > head[0 + (1*3)] && head[0 + 0] > head[1 + 0]) {
+            ans_i.push_back(1);
+            ans_j.push_back(1);
+        }
+
+        // middle cols
+        for(int j=1; j<width_1; j++) {
+            if(head[0 + (j)*3] > head[0 + (j-1)*3] &&
+               head[0 + (j)*3] > head[0 + (j+1)*3] &&
+               head[0 + (j)*3] > head[1 + (j)*3]) {
+                ans_i.push_back(1);
+                ans_j.push_back(j+1);
+            }
+        }
+
+        // top right
+        if(head[0 + (width-1)*3] > head[0 + (width-2)*3] && head[0 + (width-1)*3] > head[1 + (width-1)*3]) {
+            ans_i.push_back(1);
+            ans_j.push_back(width);
+        }
+    }
+
+    { ///between top and bottom
+        int i=0;
+/// Flattens out into three parts to avoid using i%3
+        while(true) {
+            // Part 1 - "0,1,2".  1 being the middle row
+            if(++i > height_1) break;
+            for(int j=0; j<width; j++) {
+                cin >> head[1 + j*3];
+            }
+                // leftmost col
+            if(head[1 + 0] > head[    0    ] &&
+               head[1 + 0] > head[2 + 0] &&
+               head[1 + 0] > head[1 + 3]) {
+                ans_i.push_back(2);
+                ans_j.push_back(1);
+            }
+                // middle cols
+            for(int j=1; j<width_1; j++) {
+                if(head[1 + (j)*3] > head[    (j)*3] &&
+                   head[1 + (j)*3] > head[2 + (j)*3] &&
+                   head[1 + (j)*3] > head[1 + (j-1)*3] &&
+                   head[1 + (j)*3] > head[1 + (j+1)*3]) {
+                    ans_i.push_back(i+1);
+                    ans_j.push_back(j+1);
+                }
+            }
+                // rightmost col
+            if(head[1 + width_1*3] > head[    width_1*3] &&
+               head[1 + width_1*3] > head[2 + width_1*3] &&
+               head[1 + width_1*3] > head[1 + (width_1-1)*3]) {
+                ans_i.push_back(i+1);
+                ans_j.push_back(width);
+            }
+
+            /// Part 2 - "1,2,0"
+            if(++i > height_1) break;
+            for(int j=0; j<width; j++) {
+                cin >> head[2 + j*3];
+            }
+                // leftmost col
+            if(head[2    ] > head[1    ] &&
+               head[2    ] > head[  0  ] &&
+               head[2    ] > head[2 + 3]) {
+                ans_i.push_back(i+1);
+                ans_j.push_back(1);
+            }
+                // middle cols
+            for(int j=1; j<width_1; j++) {
+                if(head[2 + (j)*3] > head[1 + (j)*3] &&
+                   head[2 + (j)*3] > head[    (j)*3] &&
+                   head[2 + (j)*3] > head[2 + (j-1)*3] &&
+                   head[2 + (j)*3] > head[2 + (j+1)*3]) {
+                    ans_i.push_back(i+1);
+                    ans_j.push_back(j+1);
+                }
+            }
+                // rightmost col
+            if(head[2 + width_1*3] > head[1 + width_1*3] &&
+               head[2 + width_1*3] > head[    width_1*3] &&
+               head[2 + width_1*3] > head[2 + (width_1-1)*3]) {
+                ans_i.push_back(i+1);
+                ans_j.push_back(width);
+            }
+
+            /// Part 3 - "2,0,1"
+            if(++i > height_1) break;
+            for(int j=0; j<width; j++) {
+                cin >> head[   j*3];
+            }
+                // leftmost col
+            if(head[0    ] > head[2    ] &&
+               head[0    ] > head[1    ] &&
+               head[0    ] > head[0 + 3]) {
+                ans_i.push_back(i+1);
+                ans_j.push_back(1);
+            }
+                // middle cols
+            for(int j=1; j<width_1; j++) {
+                if(head[0 + (j)*3] > head[2 + (j)*3] &&
+                   head[0 + (j)*3] > head[1 + (j)*3] &&
+                   head[0 + (j)*3] > head[0 + (j-1)*3] &&
+                   head[0 + (j)*3] > head[0 + (j+1)*3]) {
+                    ans_i.push_back(i+1);
+                    ans_j.push_back(j+1);
+                }
+            }
+                // rightmost col
+            if(head[0 + width_1*3] > head[2 + width_1*3] &&
+               head[0 + width_1*3] > head[1 + width_1*3] &&
+               head[0 + width_1*3] > head[0 + (width_1-1)*3]) {
+                ans_i.push_back(i+1);
+                ans_j.push_back(width);
+            }
+        }
+    }
+
+    { /// bottom row
+        if(height_1 % 3 == 1) { /// "0,1,2"
+            // bottom left
+            if(head[1 + 0] > head[0 + 0] &&
+               head[1 + 0] > head[1 + 3]) {
+                ans_i.push_back(height);
+                ans_j.push_back(1);
+            }
+            // middle cols
+            for(int j=1; j<width_1; j++) {
+                if(head[1 + j*3] > head[0 + j*3] &&
+                   head[1 + j*3] > head[1 + (j-1)*3] &&
+                   head[1 + j*3] > head[1 + (j+1)*3]) {
+                    ans_i.push_back(height);
+                    ans_j.push_back(j+1);
+                }
+            }
+            // bottom right
+            if(head[1 + width_1*3] > head[0 + width_1*3] &&
+               head[1 + width_1*3] > head[1 + (width_1-1)*3]) {
+                ans_i.push_back(height);
+                ans_j.push_back(width);
+            }
+        }
+        else if(height_1 % 3 == 2) { /// "1,2,0"
+            // bottom left
+            if(head[2 + 0] > head[1 + 0] &&
+               head[2 + 0] > head[2 + 3]) {
+                ans_i.push_back(height);
+                ans_j.push_back(1);
+            }
+            // middle cols
+            for(int j=1; j<width_1; j++) {
+                if(head[2 + j*3] > head[1 + j*3] &&
+                   head[2 + j*3] > head[2 + (j-1)*3] &&
+                   head[2 + j*3] > head[2 + (j+1)*3]) {
+                    ans_i.push_back(height);
+                    ans_j.push_back(j+1);
+                }
+            }
+            // bottom right
+            if(head[2 + width_1*3] > head[1 + width_1*3] &&
+               head[2 + width_1*3] > head[2 + (width_1-1)*3]) {
+                ans_i.push_back(height);
+                ans_j.push_back(width);
+            }
+        }
+        else { /// "2,1,0"
+            // bottom left
+            if(head[1 + 0] > head[2 + 0] &&
+               head[1 + 0] > head[1 + 3]) {
+                ans_i.push_back(height);
+                ans_j.push_back(1);
+            }
+            // middle cols
+            for(int j=1; j<width_1; j++) {
+                if(head[1 + j*3] > head[2 + j*3] &&
+                   head[1 + j*3] > head[1 + (j-1)*3] &&
+                   head[1 + j*3] > head[1 + (j+1)*3]) {
+                    ans_i.push_back(height);
+                    ans_j.push_back(j+1);
+                }
+            }
+            // bottom right
+            if(head[1 + width_1*3] > head[2 + width_1*3] &&
+               head[1 + width_1*3] > head[1 + (width_1-1)*3]) {
+                ans_i.push_back(height);
+                ans_j.push_back(width);
+            }
+        }
+    }
+
+
+
+
+    //fprintf(ofile, "%lu\n", ans_i.size());
+    ofile << ans_i.size() << endl;
+    for(std::list<int>::iterator iter_i = ans_i.begin(), iter_j = ans_j.begin();
+        iter_i != ans_i.end(); iter_i++, iter_j++) {
+        //fprintf(ofile, "%d %d\n", *iter_i, *iter_j);
+        ofile << *iter_i << " " << *iter_j << endl;
+    }
+
+    /*
+    printf("matrix: \n");
+    for(int i=0; i<height; i++) {
+        for(int j=0; j<width; j++) {
+            printf("%8d ", matrix[i][j]);
+        }
+        printf("\n");
+    }
+    */
+
+    delete[] head;
+    ifile.close();
+    ofile.close();
+}
+
+
+
+
+
 int main(int argc, char** argv)
 {
     //clock_t s = clock();
-    //Generate_matrix();
-    //Pattern_matrix();
 
-    TheF("NOPE");
+    /// assuming width>=2 && height>=3
 
-    /*
-    if(argc >= 2)
-        TheF(argv[1]);
-    else return 0xAAAAA;
-    */
+    if(argc >= 2){
+        BigRow(argv[1]);
+
+    }
+    else exit(0xAAAAAAAA);
+
 
     //printf("> %ld\n", clock() - s);
 
